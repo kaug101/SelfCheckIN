0c3880e507f009c37d472a23a7e912423e13a40a
kaug101 <77568200+kaug101@users.noreply.github.com>
2025-04-25 16:48:52 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,33 @@
-
 import streamlit as st
+import pyrebase
+
+firebase_config = st.secrets["FIREBASE_CONFIG"]
+
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
 
 def google_login():
-    st.markdown("### Sign in with Google (placeholder)")
-    user_email = st.text_input("Email")
-    if st.button("Continue"):
-        st.session_state["user_email"] = user_email
+    st.title("Sign in with Google")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    
+    login_button = st.button("Sign In")
+    register_button = st.button("Create Account")
+    
+    if login_button:
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            st.session_state["user_email"] = email
+            st.success("Logged in successfully!")
+        except:
+            st.error("Failed to login. Try again.")
+    
+    if register_button:
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            st.session_state["user_email"] = email
+            st.success("Account created!")
+        except:
+            st.error("Failed to create account. Try again.")
+    
     return st.session_state.get("user_email")
